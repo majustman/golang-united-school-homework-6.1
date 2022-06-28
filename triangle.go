a73d490d38641d4df238614cb252d39ebf35442d
samatt90 <samatt90@gmail.com>
2022-06-28 20:49:12 +0600

    init: added methods to the shapes

--- triangle.go
+++ triangle.go
@@ -1,6 +1,19 @@
 package golang_united_school_homework
 
+import "math"
+
 // Triangle must satisfy to Shape interface
 type Triangle struct {
 	Side float64
+}
+
+// CalcPerimeter returns calculation result of perimeter
+func (t *Triangle) CalcPerimeter() float64 {
+	return 3 * t.Side
+}
+
+// CalcArea returns calculation result of area
+func (t *Triangle) CalcArea() float64 {
+	s := t.CalcPerimeter() / 2
+	return math.Sqrt(3 * s * (s - t.Side))
 }
