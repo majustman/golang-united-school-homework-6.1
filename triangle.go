b5f74aaf0456d684da4c3acb855904245980f832
samatt90 <samatt90@gmail.com>
2022-06-30 09:33:33 +0600

    fix: triangle area calc

--- triangle.go
+++ triangle.go
@@ -15,5 +15,5 @@ func (t Triangle) CalcPerimeter() float64 {
 // CalcArea returns calculation result of area
 func (t Triangle) CalcArea() float64 {
 	s := t.CalcPerimeter() / 2
-	return math.Sqrt(3 * s * (s - t.Side))
+	return (math.Sqrt(3) * t.Side * t.Side) / 4
 }
